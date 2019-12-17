431b74d3f0f277e2a87e5f7c69236e43eaaf1d06
botexpert <darkodare97@gmail.com>
2019-12-17 14:14:54 +0100

    Sorting pictures is now working, but not deleting unsorted pictures! `

--- picture.py
+++ picture.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-'''Usage ./picture.py folder_to_sort known_faces_folder'''
+'''Usage ./picture.py base_folder
+prerequisites: You need to have 2 folders in base_folder. 
+1st one named 'known_faces' that contains faces with name of people as filename
+2nd one with pictures with different people that need to be sorted '''
 
 import face_recognition
 import os
 import sys
 import shutil
 from os import path
-
 
 def recognize(known_face_encoding,unknown_face_picture_path):
     '''Funkcija uporedjuje poznata i nepoznata lica i vraca niz boolean vrednosti za svako nepoznato lice 
@@ -18,11 +20,16 @@ def recognize(known_face_encoding,unknown_face_picture_path):
     unknown_face_encoding = face_recognition.face_encodings(unknown_face_picture)
     
     result = []
-    
+    res = None
     for encoding in unknown_face_encoding:
         result.append(face_recognition.compare_faces(known_face_encoding,encoding,tolerance=0.60))
     
-    return result
+    for x in result:
+        try:
+            res = x.index(True)
+        except:
+            continue    
+    return res
 
 def main(argv):
     base_dir = sys.argv[1]
@@ -31,7 +38,13 @@ def main(argv):
 
     # known_folder = '/home/botexpert/Downloads/input'
     # unknown_folder = '/home/botexpert/Downloads/output'
+
+    no_face_path = path.join(base_dir,'no_face') 
+    if not os.path.exists(no_face_path):
+        os.makedirs(no_face_path)
+    
     newpath = path.join(base_dir,'unknown') 
+    
     if not os.path.exists(newpath):
         os.makedirs(newpath)
 
@@ -39,7 +52,9 @@ def main(argv):
     dir_unknown_files = os.listdir(unknown_folder)
     
     known_faces = []
+    known_map = dict()
 
+    map_index=0
     for kf in dir_known_files:
         '''Dobijanje niza poznatih lica'''
         
@@ -48,31 +63,32 @@ def main(argv):
         try:
             known_face_picture = face_recognition.load_image_file(known_path)
             known_faces.append(face_recognition.face_encodings(known_face_picture)[0])
+
+            filename,extension = path.splitext(kf)
+            known_map[map_index] = filename
+            map_index += 1
+            new_folder_for_sorted= path.join(base_dir,filename)
+            if not os.path.exists(new_folder_for_sorted):     #kreira folder za svaku pozantu facu
+                os.makedirs(new_folder_for_sorted)
+        
         except(IndexError):
-            no_face_path = path.join(base_dir,'no_face') 
-            if not os.path.exists(no_face_path):
-                os.makedirs(no_face_path)
-                continue
+            shutil.move(known_path,no_face_path)
+            continue
 
 
     for ukf in dir_unknown_files:
         unknown_path = path.join(unknown_folder,ukf)
-
         found_faces = recognize(known_faces,unknown_path)
-        # print("FACES: {} ".format(found_faces))
-    
-    if any() in found_faces:
+        print(found_faces)   
+        if found_faces != None:
+            where = path.join (base_dir,known_map.get(found_faces))
+            print(where)
 
-    else:
-        shutil.move(,no_face_path)
-
-
-
-
-
-
-
+            shutil.copy(unknown_path,where)
+        else:
+            shutil.copy(unknown_path,path.join(base_dir,'unknown'))
 
 if __name__ == "__main__":
     main(sys.argv)
+    print("DONE !!!!!!!!!!")
     pass
